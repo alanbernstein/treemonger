ae8f0e8d2b1f5031d85dfc778d8cd76bb5b3c703
Alan Bernstein <abernstein@umbel.com>
2016-10-06 22:45:33 -0500

    improve format_bytes function

--- utils.py
+++ utils.py
@@ -10,12 +10,14 @@ _abbrevs = [(1 << 50, 'P'),
 
 
 def format_bytes(size):
-    """Return a string representing the metric suffix of a size"""
+    """Return a human readable size string (i.e., kB, MB, etc)"""
+    k = 2.0
+    # this makes the jump occur at 2kB instead of 1kB, which makes thing a little more readable
     for factor, suffix in _abbrevs:
-        if size > factor:
+        if size > k * factor:
             break
     # return `int(size/(1.0*factor))` + suffix
-    s = "%.2f%s" % (size / (1.0 * factor), suffix)
+    s = "%.2f%sB" % (size / (1.0 * factor), suffix)
     return s
 
 
